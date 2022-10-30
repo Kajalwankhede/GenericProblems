f5ff731482bc53b0c46f97fa8890fcfd69be49c2
Kajakwankhede <Kajalwankhede@example.com>
2022-10-30 16:46:41 +0530

    Added all 3 to one generic method and find maximum

--- src/com/generics/FindMaximumUsingGeneric.java
+++ src/com/generics/FindMaximumUsingGeneric.java
@@ -1,6 +1,9 @@
 package com.generics;
-public class FindMaximumUsingGeneric{
-    public static <T extends Comparable<T>> T maximum(T a, T b, T c) // shows the largest of 3 comaptible objects
+public class FindMaximumUsingGeneric <T extends Comparable<T>>{
+    T a,b,c;
+
+    public static <T extends Comparable<T>>
+    T maximum(T a, T b, T c) // shows the largest of 3 comaptible objects
     {
         T max = a;
 
@@ -15,24 +18,30 @@ public class FindMaximumUsingGeneric{
     }
 
     public static void main(String args[]) {
-      //  System.out.println("\nGenerics To test Maximum of 3 Variables.");
-      //  System.out.printf("Maximum Integers of %d, %d and %d \nNumber is: %d\n\n",
-          //      8, 4, 10, maximum( 8, 4, 10 ));
+        System.out.println("\nGenerics To test Maximum of 3 Variables.");
+         System.out.printf("Maximum Integers of %d, %d and %d \nInteger Number At First Position :%d\n\n",
+                  8, 4, 1, maximum( 8, 4, 1 ));
+        System.out.printf("Maximum Integers of %d, %d and %d \nInteger Number At Second Position : %d\n\n",
+                8, 9, 1, maximum( 8, 9, 1 ));
+        System.out.printf("Maximum Integers of %d, %d and %d \nInteger Number At Third Position : %d\n\n",
+                8, 4, 10, maximum( 8, 4, 10 ));
 
-      //  System.out.printf("Maximum Float Number of %.1f,%.1f and %.1f \nFloat Number At First Position: %.1f\n\n",
-            //    8.8, 1.1, 2.2, maximum(   8.8,1.1, 2.2));
-       // System.out.printf("Maximum Float Number of %.1f,%.1f and %.1f \nFloat Number At Second Position %.1f\n\n",
-            //    8.8, 9.9, 2.2, maximum(   8.8,9.9, 2.2));
+        System.out.printf("Maximum Float Number of %.1f,%.1f and %.1f \nFloat Number At First Position: %.1f\n\n",
+          8.8, 1.1, 2.2, maximum(   8.8,1.1, 2.2));
 
-      //  System.out.printf("Maximum Float Number of %.1f,%.1f and %.1f \nFloat Number At Third Position: %.1f\n\n",
-         //       7.7, 6.6, 9.9, maximum(    7.7, 6.6, 9.9));
-  System.out.printf("Maximum String of %s, %s and %s\nString At First Position: %s\n","Peach",
+          System.out.printf("Maximum Float Number of %.1f,%.1f and %.1f \nFloat Number At Second Position %.1f\n\n",
+         8.8, 9.9, 2.2, maximum(   8.8,9.9, 2.2));
+
+           System.out.printf("Maximum Float Number of %.1f,%.1f and %.1f \nFloat Number At Third Position: %.1f\n\n",
+               7.7, 6.6, 9.9, maximum(    7.7, 6.6, 9.9));
+
+          System.out.printf("Maximum String of %s, %s and %s\nString At First Position: %s\n","Peach",
             "Apple", "Banana", maximum("Peach", "Apple", "Banana"));
 
-    System.out.printf("Maximum String of %s, %s and %s\nString At Second Position: %s\n","Apple",
+           System.out.printf("Maximum String of %s, %s and %s\nString At Second Position: %s\n","Apple",
             "Peach", "Banana", maximum("Apple", "Peach", "Banana"));
 
-        System.out.printf("Maximum String of %s, %s and %s\nString At Third Position: %s\n","Apple",
+              System.out.printf("Maximum String of %s, %s and %s\nString At Third Position: %s\n","Apple",
                 "Banana", "Peach", maximum("Apple", "Banana", "Peach"));
 
 
