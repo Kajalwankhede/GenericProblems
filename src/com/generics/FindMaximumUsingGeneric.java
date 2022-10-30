6317bc0814c2bb8dab988db2f48420e4e4db8379
Kajakwankhede <Kajalwankhede@example.com>
2022-10-30 17:57:57 +0530

    Added printmax generic method which is called from testmaximum

--- src/com/generics/FindMaximumUsingGeneric.java
+++ src/com/generics/FindMaximumUsingGeneric.java
@@ -41,20 +41,19 @@ public class FindMaximumUsingGeneric <T extends Comparable<T>>{
 
     public static <T> void printMax(T a, T b, T c, T max){
 
-        System.out.printf("Maximum of %s, %s and %s is:  %s\n",a,b,c,max);
+        System.out.printf("Maximum Of the  %s, %s and %s is:  %s\n",a,b,c,max);
     }
 
     public static void main(String args[]) {
         System.out.println("\nGenerics To test Maximum of 3 Variables.");
-        Integer aInt=3, bInt=4, cInt=5;
-        Float aF1=6.6f,bF1=8.8f,cF1=7.7f;
-        String aStr="pear",bStr="apple",cStr="Orange";
+        Integer aInt = 8, bInt = 4, cInt = 5;
+        Float aF1 = 6.6f, bF1 = 8.8f, cF1 = 7.7f;
+        String aStr = "peach", bStr = "apple", cStr = "banana";
 
-        FindMaximumUsingGeneric.testMaximum(aStr,bStr,cStr);
-        new FindMaximumUsingGeneric(aInt,bInt,cInt).maximum();
-        new FindMaximumUsingGeneric(aF1,bF1,cF1).maximum();
-        new FindMaximumUsingGeneric(aStr,bStr,cStr).maximum();
-
+        FindMaximumUsingGeneric.testMaximum(aStr, bStr, cStr);
+        new FindMaximumUsingGeneric(aInt, bInt, cInt).maximum();
+        new FindMaximumUsingGeneric(aF1, bF1, cF1).maximum();
+        new FindMaximumUsingGeneric(aStr, bStr, cStr).maximum();
 
     }
-}
+    }
