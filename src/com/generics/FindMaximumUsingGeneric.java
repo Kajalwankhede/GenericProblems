55c07e00991aa6652b2e2c83d35b977b0981aef6
Kajakwankhede <Kajalwankhede@example.com>
2022-10-30 15:44:08 +0530

    Added to find max float number at 1st position return same number

--- src/com/generics/FindMaximumUsingGeneric.java
+++ src/com/generics/FindMaximumUsingGeneric.java
@@ -15,9 +15,12 @@ public class FindMaximumUsingGeneric{
     }
 
     public static void main(String args[]) {
-        System.out.println("\nGenerics To test MAximum of 3 Variables.");
-        System.out.printf("Maximum of %d, %d and %d \nNumber is: %d\n\n",
+        System.out.println("\nGenerics To test Maximum of 3 Variables.");
+        System.out.printf("Maximum Integers of %d, %d and %d \nNumber is: %d\n\n",
                 8, 4, 10, maximum( 8, 4, 10 ));
+
+        System.out.printf("Maximum Float Number of %.1f,%.1f and %.1f \nFloat Number is: %.1f\n\n",
+                8.8, 1.1, 2.2, maximum(   8.8,1.1, 2.2));
 
     }
 }
