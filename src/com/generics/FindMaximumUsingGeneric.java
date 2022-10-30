4f1e3c8775aef47c70175653d4595b75dc9d6155
Kajakwankhede <Kajalwankhede@example.com>
2022-10-30 16:24:07 +0530

    Added to find max string  at 3rd  position return same String

--- src/com/generics/FindMaximumUsingGeneric.java
+++ src/com/generics/FindMaximumUsingGeneric.java
@@ -15,17 +15,25 @@ public class FindMaximumUsingGeneric{
     }
 
     public static void main(String args[]) {
-        System.out.println("\nGenerics To test Maximum of 3 Variables.");
-        System.out.printf("Maximum Integers of %d, %d and %d \nNumber is: %d\n\n",
-                8, 4, 10, maximum( 8, 4, 10 ));
+      //  System.out.println("\nGenerics To test Maximum of 3 Variables.");
+      //  System.out.printf("Maximum Integers of %d, %d and %d \nNumber is: %d\n\n",
+          //      8, 4, 10, maximum( 8, 4, 10 ));
 
-        System.out.printf("Maximum Float Number of %.1f,%.1f and %.1f \nFloat Number At First Position: %.1f\n\n",
-                8.8, 1.1, 2.2, maximum(   8.8,1.1, 2.2));
-        System.out.printf("Maximum Float Number of %.1f,%.1f and %.1f \nFloat Number At Second Position %.1f\n\n",
-                8.8, 9.9, 2.2, maximum(   8.8,9.9, 2.2));
+      //  System.out.printf("Maximum Float Number of %.1f,%.1f and %.1f \nFloat Number At First Position: %.1f\n\n",
+            //    8.8, 1.1, 2.2, maximum(   8.8,1.1, 2.2));
+       // System.out.printf("Maximum Float Number of %.1f,%.1f and %.1f \nFloat Number At Second Position %.1f\n\n",
+            //    8.8, 9.9, 2.2, maximum(   8.8,9.9, 2.2));
 
-        System.out.printf("Maximum Float Number of %.1f,%.1f and %.1f \nFloat Number At Third Position: %.1f\n\n",
-                7.7, 6.6, 9.9, maximum(    7.7, 6.6, 9.9));
+      //  System.out.printf("Maximum Float Number of %.1f,%.1f and %.1f \nFloat Number At Third Position: %.1f\n\n",
+         //       7.7, 6.6, 9.9, maximum(    7.7, 6.6, 9.9));
+  System.out.printf("Maximum String of %s, %s and %s\nString At First Position: %s\n","Peach",
+            "Apple", "Banana", maximum("Peach", "Apple", "Banana"));
+
+    System.out.printf("Maximum String of %s, %s and %s\nString At Second Position: %s\n","Apple",
+            "Peach", "Banana", maximum("Apple", "Peach", "Banana"));
+
+        System.out.printf("Maximum String of %s, %s and %s\nString At Third Position: %s\n","Apple",
+                "Banana", "Peach", maximum("Apple", "Banana", "Peach"));
 
 
     }
